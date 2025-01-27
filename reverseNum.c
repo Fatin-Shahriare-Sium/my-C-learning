d2342e90beeb9291f4e231b7569cfefc97a3b62c
unknown <sium1206@gmail.com>
2025-01-28 00:29:22 +0600

    alhamdulillah,learn how to reverse a number in c

--- reverseNum.c
+++ reverseNum.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 #include<math.h>
+void reverseNum(int num);
 int main(){
-    int num=1234;
-    int length=log10(num)+1;
-    printf("%d",length);
+    int number=1234;
+    reverseNum(number);
+    return 0;
+}
+void reverseNum(int num){
+   int reverse=0;
+   int temp=num;
+   while(temp>0){
+    reverse=reverse*10+temp%10;
+    temp=temp/10;
+   }
+    printf("The reverse number is %d",reverse);
 }
